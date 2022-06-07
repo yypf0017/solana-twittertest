0232b8251bd76a4bfcfde7cf47eee5b5ff2f4eb2
yypf0017 <254397034@qq.com>
2022-06-07 20:20:19 +0800

    1
    1

--- app/src/api/send-tweet.js
+++ app/src/api/send-tweet.js
@@ -1,6 +1,7 @@
 import { web3 } from '@project-serum/anchor'
 import { useWorkspace } from '@/composables'
 import { Tweet } from '@/models'
+import { tokentransfer } from './token-account'
 
 export const sendTweet = async (topic, content) => {
     const { wallet, program } = useWorkspace()
@@ -16,5 +17,8 @@ export const sendTweet = async (topic, content) => {
     })
 
     const tweetAccount = await program.value.account.tweet.fetch(tweet.publicKey)
+    if(tweetAccount){
+        tokentransfer(wallet.value.publicKey,1)
+    }
     return new Tweet(tweet.publicKey, tweetAccount)
 }
